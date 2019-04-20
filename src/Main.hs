61191f608333178e2eb5ec1b086f055c14ad7320
threestrikes <wontez@protonmail.com>
2019-04-20 11:22:22 -0400

    Learned  that autosave makes it easier to get instant feedback

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+myName :: [Char]
+myName = "Wontez"
+
+myNameRev :: [Char]
+myNameRev = reverse myName
+
 main :: IO ()
 main = do
   putStrLn "hello world"
-
+  putStrLn "I get automatic feedback"
+  putStrLn myNameRev
